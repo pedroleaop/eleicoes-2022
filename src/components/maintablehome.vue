f41250cbdad2c82b832fc5c0f3d706b12c5d4162
pedroleaop <pedroleaop@hotmail.com>
2021-03-11 21:38:16 -0300

    maintable

--- src/components/maintablehome.vue
+++ src/components/maintablehome.vue
@@ -2,17 +2,81 @@
 <div>
  <h2>Últimas notícias</h2>
  <router-view/>
+
+ <tr v-for="noticia of noticias" :key="noticia.id"> 
+ <td><v-alert
+      border="left"
+      color="grey lighten-2"
+      dark>
+        <img id="imgnoticia" :src="noticia.picture">
+      <div id="manchete">{{noticia.manchete}}</div>
+      <div id="buttonnot" class="text-center">
+    <a href="www.google.com"><v-btn
+      rounded
+      color="primary"
+      dark
+    >
+     Leia mais
+    </v-btn></a>
+  </div>     
+    </v-alert></td>
+    </tr>
+    
 </div>
 </template>
 
 <script>
 export default {
     name: 'maintablehome',
+    data() {
+        return {
+            noticias: []
+        }
+    },
+    created() {
+        fetch('https://raw.githubusercontent.com/pedroleaop/eleicoes-2022/main/src/assets/noticias.json')
+        .then(response => response.json())
+        .then(json => {
+            this.noticias = json;
+        })
+    }
 }
 </script>
 
 <style scoped>
+
+.theme--dark.v-sheet {
+    color:rgb(59, 59, 59);
+}
+
+.v-alert {
+    font-size: 18px;
+    font-weight: bold;
+    max-height: 150px;
+}
+
 h2 {
     text-align: left;
+}
+img {
+    max-width: 10vw;
+}
+
+#manchete {
+    position: relative;
+    top: -75px;
+    padding-left: 25vw;
+}
+
+#buttonnot {
+    position: relative;
+    top: -60px;
+    right: -90px;
+}
+
+#imgnoticia {
+    display: flex;
+    max-width: 25vw;
+    min-height: 25vw;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
- /* {
+/*  {
     path: '/',
     name: 'Home',
     component: Home
@@ -12,10 +12,12 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-   
-    component: () => import('../views/About.vue')
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( '../views/About.vue')
   } */
-] 
+]
 
 const router = new VueRouter({
   mode: 'history',
